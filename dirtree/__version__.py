8854f389afedf6aa467af6f4ccd4266153b91c9a
Brandon LeBlanc <brandon@leblanc.codes>
2018-05-14 17:45:22 -0500

    change to verbose unicode strings

--- dirtree/__version__.py
+++ dirtree/__version__.py
@@ -8,4 +8,5 @@ __author__ = 'Brandon LeBlanc'
 __author_email__ = 'brandon@leblanc.codes'
 __license__ = 'Apache 2.0'
 __copyright__ = 'Copyright 2018 LeBlanc Code, LLC'
-__coffee__ = '\u26a1 + \u2615 = \U0001F60D \u2764\ufe0f'
+# idk why, but seeing the unicode names makes me so happy
+__coffee__ = '\N{High Voltage Sign} + \N{Hot Beverage} = \N{Smiling Face with Heart-Shaped Eyes} \N{Heavy Black Heart}\N{variation selector-16}'
