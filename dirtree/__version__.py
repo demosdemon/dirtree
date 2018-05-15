999c13ab24c9479450e2783a5f257ca84d904f61
Brandon LeBlanc <brandon@leblanc.codes>
2018-05-14 21:04:05 -0500

    line too long, wasn't worth the verbosity

--- dirtree/__version__.py
+++ dirtree/__version__.py
@@ -8,5 +8,4 @@ __author__ = 'Brandon LeBlanc'
 __author_email__ = 'brandon@leblanc.codes'
 __license__ = 'Apache 2.0'
 __copyright__ = 'Copyright 2018 LeBlanc Code, LLC'
-# idk why, but seeing the unicode names makes me so happy
-__coffee__ = '\N{High Voltage Sign} + \N{Hot Beverage} = \N{Smiling Face with Heart-Shaped Eyes} \N{Heavy Black Heart}\N{variation selector-16}'
+__coffee__ = '\u26a1 + \u2615 = \U0001f60d \u2764\ufe0f'
